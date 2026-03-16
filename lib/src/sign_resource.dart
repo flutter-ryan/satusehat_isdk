8d1a513d79214e96d68ae0591fc68efa3971b178
Ryan Achmad <rhean.achmad@gmail.com>
2026-03-17 04:21:54 +0800

    disable explicit hashing

--- lib/src/sign_resource.dart
+++ lib/src/sign_resource.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
-import 'package:crypto/crypto.dart';
 import 'package:satusehat_isdk/src/canonicalize.dart';
 import 'package:satusehat_isdk/src/secure_keystore.dart';
 
@@ -12,9 +11,6 @@ class SignResource {
     /// RFC 8785
     String jsonCanonical = canonicalize(data);
     final bytes = utf8.encode(jsonCanonical);
-
-    /// Explicit SHA256 hashing
-    // final digest = sha256.convert(bytes);
 
     /// Signing hashing json/document
     Uint8List signingData = Uint8List.fromList(bytes);
